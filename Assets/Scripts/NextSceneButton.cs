38de91379717794bc11960cffe02b2cca141e2d1
Aman Kumar Matta <amankumarmatta0@gmail.com>
2023-03-04 10:00:44 +0530

    Fixed minor bugs

---
+++ Assets/Scripts/NextSceneButton.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextSceneButton : MonoBehaviour
+{
+    public Button nextSceneButton;
+    public Button Home;
+    public Button tryAgain;
+
+    void Start()
+    {
+        nextSceneButton.onClick.AddListener(LoadNextScene);
+        Home.onClick.AddListener(HomeBtn);
+        tryAgain.onClick.AddListener(TryAgain);
+    }
+
+    public void LoadNextScene()
+    {
+        int lastSceneIndex = SceneManager.sceneCountInBuildSettings - 1;
+        int nextSceneIndex = lastSceneIndex + 1;
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+
+    public void HomeBtn()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(lastSceneIndex);
+    }
+}
