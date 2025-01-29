5a66879e4068a003e826a95f1cb6c58028e89a08
Aman K <amankumarmatta0@gmail.com>
2025-01-30 01:33:43 +0530

    Imported DOTween, Added Button Anims in Start Menu

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,11 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button quitButton; 
+    private void Start()
+    {
+        startButton.onClick.AddListener(() => AnimateButton(startButton, StartGame));
+        settingsButton.onClick.AddListener(() => AnimateButton(settingsButton, OpenSettings));
+        quitButton.onClick.AddListener(() => AnimateButton(quitButton, QuitGame));
+    }
+
+    private void AnimateButton(Button button, Action callback)
+    {
+        button.transform.DOScale(0.8f, 0.1f).OnComplete(() =>
+        {
+            button.transform.DOScale(1f, 0.1f).OnComplete(() => callback());
+        });
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("Level Select");
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Settings");
     }
 
     public void QuitGame()
